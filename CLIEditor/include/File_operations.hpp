ebe94e7af4f30f22a0daf69eee80d83cb669958e
mamutrahal269 <levrymvc@gmail.com>
2025-05-20 20:03:06 +0700

    Update File_operations.hpp

--- CLIEditor/include/File_operations.hpp
+++ CLIEditor/include/File_operations.hpp
@@ -5,7 +5,7 @@
 namespace cliedit{
 	
 	int read(std::fstream& file);
-	int search(std::fstream& file,std::string str);
+	int search(std::fstream& file,const std::string& str);
 	int copy(std::fstream& file1,std::fstream& file2);
 	
 }
