b56c8962288b97b826af3b9d9c63f4119375dc78
mamutrahal269 <levrymvc@gmail.com>
2025-05-20 19:57:57 +0700

    Update File_edit.hpp

--- CLIEditor/include/File_edit.hpp
+++ CLIEditor/include/File_edit.hpp
@@ -3,7 +3,7 @@
 #include <fstream>
 namespace cliedit{
 	
-	int write(std::fstream& file,std::string str);
+	int write(std::fstream& file,const std::string& str);
 	int erase(const std::string& filename,int start,int count);
 	int paste(const std::string& file,const std::string& str,int byte);
 	
