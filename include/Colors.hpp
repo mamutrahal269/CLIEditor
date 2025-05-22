7bda3794b4ee8f640b8ee152f0db3c0c731cb7fc
mamutrahal269 <levrymvc@gmail.com>
2025-05-23 00:29:18 +0700

    Add files via upload

---
+++ include/Colors.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <string>
+
+const std::string RED("\033[91m");
+const std::string YELLOW("\033[93m");
+const std::string GREEN("\033[92m");
+const std::string RESET("\033[0m");
